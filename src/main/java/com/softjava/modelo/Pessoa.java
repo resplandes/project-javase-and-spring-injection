cae0cb2aa986513235a58be9a9ce076581e7ab7d
Willian Resplandes Matias <willianresplandes@gmail.com>
2018-04-17 16:02:20 -0400

    Inicio Samuel verificar

---
+++ src/main/java/com/softjava/modelo/Pessoa.java
@@ -0,0 +1,44 @@
+package com.softjava.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String nome;
+
+	public Pessoa() {
+	}
+
+	public Pessoa(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome	;
+	}
+
+}
